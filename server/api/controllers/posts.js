2ea048e2da943d4eb3ba499b50411f14ae7d4a9b
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-04 15:38:54 +0100

    add / route to controllers

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const Post = require('../models/post')
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.all
+        res.json({posts})
+    } catch(err) {
+        res.status(500).json({err})
+    }
+})
+
+
+
+
 module.exports = router;
