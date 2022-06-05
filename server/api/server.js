6a8374011e1e0edcc8cca30234d81c827de85ba9
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-05 19:00:05 +0100

    add auth/user routes

--- server/api/server.js
+++ server/api/server.js
@@ -8,6 +8,12 @@ server.use(express.json());
 const postRoutes = require('./controllers/posts')
 server.use('/posts', postRoutes)
 
+const userRoutes = require('./controllers/users')
+server.use('/users', userRoutes)
+
+const authRoutes = require('./controllers/auth');
+server.use('/auth', authRoutes);
+
 // Root route
 server.get('/', (req, res) => res.send('Hello, client!'))
 
